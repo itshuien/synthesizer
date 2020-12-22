961ad82657c437381de2a77d1f842a27d01aae9b
itshuien <huien_wong@hotmail.com>
2020-12-22 16:43:44 +0800

    Move style jsx to scss file

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">Synthesizer</h1>
-        <div className="chords-wrapper">
+        <div style={{ display: 'flex' }}>
           <Chord chordName="g" notes={['G3', 'Bb3', 'D3']} />
           <Chord chordName="a" notes={['A3', 'C4', 'E4']} />
           <Chord chordName="Bb" notes={['Bb3', 'D4', 'F4']} />
@@ -22,48 +22,6 @@ export default function Home() {
 
         <Keyboard />
       </main>
-
-      <style jsx>{`
-        .chords-wrapper {
-          display: flex;
-        }
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        .title {
-          margin-bottom: 24px;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          background-color: #eeeeee;
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
